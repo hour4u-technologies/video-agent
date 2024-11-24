273e723936f6ad4d0e682c0717606b08d622dc79
Hour4u technologies private limited <69034244+hour4u-technologies@users.noreply.github.com>
2024-11-24 15:53:08 +0530

    reducing "top_k": 64 to 40

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def initialize_gemini_model():
     generation_config = {
         "temperature": 1,
         "top_p": 0.95,
-        "top_k": 64,
+        "top_k": 40,
         "max_output_tokens": 8192,
         "response_mime_type": "text/plain",
     }
